cc072e6e8b768c7b48de206649e9aba2f1fdae75
keettlynn <66968435+keettlynn@users.noreply.github.com>
2021-01-30 21:08:49 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,6 @@
+const button = document.querySelector("button");
+
+button.onclick = function () {
+  let name = prompt("Who loves you the most?");
+  alert("Yes, " + name + ", truly loves you the most!");
+};
